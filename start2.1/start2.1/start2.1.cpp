60da66fa68d841ef140ab3448353e4c780228850
akon allayar <akonaxvy@gmail.com>
2025-10-02 12:46:36 +0500

    ass

---
+++ start2.1/start2.1/start2.1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	string name, gender;int age;float height;double weight; bool isStudent;
+	cout << "Enter your name: "; cin >> name;
+	cout << "Enter your age: "; cin >> age;
+	cout << "Enter your height (in meters): "; cin >> height;
+	cout << "Enter your weight (in kg): "; cin >> weight;
+	cout << "Type 1 if you are a student, otherwise type 0: "; cin >> isStudent;
+}
