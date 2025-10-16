fed576e7787fa1cf437b3265f1a88cba51e50ce6
akon allayar <akonaxvy@gmail.com>
2025-10-16 12:19:09 +0500

    16.10

---
+++ Start3.1_if_ticket/Start3.1_if_ticket/Start3.1_if_ticket.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int age,tick;
+	cout << "Enter your age: ";
+	cin >> age;
+	if (age >= 18){
+		cout << "Do you have a ticket?(1-yes,2-no)";
+		cin >> tick;
+		if (tick == 1){
+			cout << "Welcome to the cinema!!!" << endl;
+		} 
+		else {
+			cout << "Buy a ticket!!!" << endl;
+		}
+	} 
+	else {
+		cout << "Minnimum from 18!!!" << endl;
+	}
+	return 0;
+}
---
+++ Start_2.7_temp/Start_2.7_temp/Start_2.7_temp.cpp
@@ -0,0 +1,21 @@
+﻿// Start_2.7_temp.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(UTF - 8);
+	int temp;
+	cout << "Введите температуру в градусах Цельсия: ";
+	cin >> temp;
+	if (temp > 25) {
+		cout << "На улице тепло,возмите воду" << endl;
+	}
+	else if (temp < 25) {
+		cout << "На улице холодно, наденьте куртку" << endl;
+	}
+	else {
+		cout << "цифрами" << endl;
+	}
